b20594766c0d4cc3c62353ef44a511e1df068c28
Thomas Van Holder <thomasvanholder@gmail.com>
2020-02-18 17:01:50 -0300

    config models routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # # Read all
+  # get "tasks", to: "tasks#index", as: :tasks
+  # # Create
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # # Read one
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # # Update
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # patch "tasks/:id", to: "tasks#update"
+  # # Delete
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
